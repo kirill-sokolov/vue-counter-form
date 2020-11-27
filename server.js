1e438132dd1fa57c64569df832ef315b29ae71c9
Kirill <kirill.sokolov.lv@gmail.com>
2020-11-27 19:53:48 +0200

    fix heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('./dist'));
 app.get('/*',  (req, res) => {
-  res.sendFile('index.html', {root: 'dist'});
+  res.sendFile('index.html', {root: 'dist/'});
 });
 app.listen(port);
 console.log('working on ' + port);
