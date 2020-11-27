7a27d6122f34eacadd8dd5f133dfa48020d313c9
Kirill <kirill.sokolov.lv@gmail.com>
2020-11-27 18:20:07 +0200

    add server.js for running on heroku

---
+++ server.js
@@ -0,0 +1,9 @@
+let port = process.env.PORT || process.argv[2] || 5280;
+const express = require('express');
+const app = express();
+app.use(express.static('./dist'));
+app.get('/*',  (req, res) => {
+  res.sendFile('index.html', {root: 'dist'});
+});
+app.listen(port);
+console.log('working on ' + port);
